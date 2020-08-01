14e3caf84569db7e57e3263665abb4b1d8db72dc
Tony Sorensen <tony48853@gmail.com>
2020-08-01 10:30:59 -0700

    Card component created, props being passed from App to Character

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,32 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
+import CharacterCard from "./components/Character";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [character, setCharacter] = useState([]);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-useEffect(() => {
-  axios
-  .get("https://swapi.dev/api/vehicles/")
-  .then(swapiData => {
-    console.log('SWAPI DATA', swapiData)
-  })
-  .catch(error => {
-    console.log('The data was not returned.', error)
-  })
-},[])
-
-
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then(response => {
+        console.log(response)
+        const characterList = response.data.results
+         console.log("Response DATA", characterList);
+        setCharacter(characterList)
+      })
+      .catch(error => {
+        console.log("The data was not returned.", error);
+      })
+  }, []);
+  
   return (
+    <>
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">What a bunch of Characters</h1>
     </div>
-  )
-}
+    <div className="cardHolder">
+    {character.map(who => {
+          return <CharacterCard key={who.url} name= {who.name}  />;
+        })}
+    </div>
+    </>
+  );
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const NewCard = styled.div`
+  background: #3f537e;
+  color: white;
+  width: 200px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const CharacterCard = (props) => {
+  return (
+    <NewCard>
+      <h2>{props.name}</h2>
+    </NewCard>
+  );
+};
+
+export default CharacterCard;
