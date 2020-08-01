31d081d42691c273bf42c43dc43437753cadc937
Tony Sorensen <tony48853@gmail.com>
2020-08-01 10:49:21 -0700

    Added Physical traits to Character Card, calling data from axios info. Names and cards displaying but need to be styled and have positioning fixed.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import "./App.css";
 import CharacterCard from "./components/Character";
 
-
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -15,27 +14,38 @@ const App = () => {
   useEffect(() => {
     axios
       .get("https://swapi.dev/api/people/")
-      .then(response => {
-        console.log(response)
-        const characterList = response.data.results
-         console.log("Response DATA", characterList);
-        setCharacter(characterList)
+      .then((response) => {
+        console.log(response);
+        const characterList = response.data.results;
+        console.log("Response DATA", characterList);
+        setCharacter(characterList);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("The data was not returned.", error);
-      })
+      });
   }, []);
-  
+
   return (
     <>
-    <div className="App">
-      <h1 className="Header">What a bunch of Characters</h1>
-    </div>
-    <div className="cardHolder">
-    {character.map(who => {
-          return <CharacterCard key={who.url} name= {who.name}  />;
+      <div className="App">
+        <h1 className="Header">What a bunch of Characters</h1>
+      </div>
+      <div className="cardHolder">
+        {character.map((who) => {
+          return (
+            <CharacterCard
+              key={who.url}
+              name={who.name}
+              eyecolor={who.eye_color}
+              gender={who.gender}
+              haircolor={who.hair_color}
+              height={who.height}
+              mass={who.mass}
+              skincolor={who.skin_color}
+            />
+          );
         })}
-    </div>
+      </div>
     </>
   );
 };
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,23 +2,43 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+`;
 const NewCard = styled.div`
   background: #3f537e;
   color: white;
-  width: 200px;
+  width: 400px;
   height: 300px;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: start;
   justify-content: space-between;
   margin-bottom: 20px;
 `;
 
 const CharacterCard = (props) => {
   return (
-    <NewCard>
-      <h2>{props.name}</h2>
-    </NewCard>
+    <>
+      <Container>
+        <h2>{props.name}</h2>
+        <NewCard>
+          <ul>
+            <h4>Physical Traits</h4>
+            <li>Eye Color: {props.eyecolor} </li>
+            <li>Gender: {props.gender}</li>
+            <li>Hair Color: {props.haircolor} </li>
+            <li>Height: {props.height}</li>
+            <li>Mass: {props.mass}</li>
+            <li>Skin Color: {props.skincolor}</li>
+          </ul>
+        </NewCard>
+      </Container>
+    </>
   );
 };
 
